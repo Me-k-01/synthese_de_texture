609e1e02dd55b4a79b88fe29d40aea79c7c412ed
Me-k-01 <mecaman01@gmail.com>
2022-12-06 09:58:04 +0100

    Perm init

--- permuteur.cpp
+++ permuteur.cpp
@@ -1,26 +1,20 @@
 #include "permuteur.h"
 #include <vector>
 #include <random>
-#include <iostream>
 
-Permuteur::Permuteur(int max) { 
-//Permuteur::Permuteur(int max):i_perm(0),this->max(max) { // Je sais pas ce qu'il manque sur cette ligne que Timothé a fait, donc je commente en attendant
-    i_perm = 0;
-    this->max = max;
-
-    perm = new int[max]; // permutation courante
+Permuteur::Permuteur(int amax):max(amax),i_perm(0) { // Je sais pas ce qu'il manque sur cette ligne que Timothée a fait, donc je commente en attendant
+    perm = new int[max]; // Permutation courante
     indices = new int[max]; // Indices possibles
 
     for(int i=0; i<max; i++){
         indices[i] = i;
         perm[i] = i;
     } 
-    
+
     generePerm(); // Première permutation
 }
 
-Permuteur::~Permuteur() {
-}
+Permuteur::~Permuteur() {}
 
 void Permuteur::generePerm() { 
     for (int indMax = max-1; indMax > 0; indMax --) { 
@@ -34,31 +28,6 @@ void Permuteur::generePerm() {
     }  
 }
 
-/*
-void Permuteur::generateFlo() {
-    int ind = max;
-    Vector<int> possibleIndices ; // TODO: initialiser
-    for (int i = 0; i<max; i++) {
-        int indRdm = random()%possibleIndices.size();
-        perm[i] = possibleIndices[indRdm];
-        possibleIndices.remove(ind)
-    }  
-}
-
-
-
-void Permuteur::generePermRom() {
-    int deja_present[max];
-    for(int i=0;i<max;i++){
-        int n = random() % (max);
-        while(std::count(begin(deja_present), end(deja_present), n)!=0){
-            n=random() % (max);
-        }
-        perm[i]=n;
-        deja_present[i]=n;
-    }
-}
-*/
 
 int Permuteur::suivant(){
     i_perm ++;
